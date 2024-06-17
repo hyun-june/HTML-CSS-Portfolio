69a2c44c0656115eae91fc0c896b19f982c5158c
Hyunjun Lee <leehyunjune11@gmail.com>
2024-06-18 01:00:16 +0900

    modal form

---
+++ main.js
@@ -0,0 +1,20 @@
+const Modal1 = new bootstrap.Modal(document.getElementById('Modal-1'));
+const Modal2 = new bootstrap.Modal(document.getElementById('Modal-2'));
+const Modal3 = new bootstrap.Modal(document.getElementById('Modal-3'));
+const Modal4 = new bootstrap.Modal(document.getElementById('Modal-4'));
+
+const playModal1 = () => {
+    Modal1.show();
+}
+
+const playModal2 = () => {
+    Modal2.show();
+}
+
+const playModal3 = () => {
+    Modal3.show();
+}
+
+const playModal4 = () => {
+    Modal4.show();
+}
