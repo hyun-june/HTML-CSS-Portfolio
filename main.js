522b66326a1bf755a8eb0d76660505b0dab4c7e3
Hyunjun Lee <leehyunjune11@naver.com>
2024-06-18 18:08:03 +0900

    closebtn update

--- main.js
+++ main.js
@@ -2,19 +2,36 @@ const Modal1 = new bootstrap.Modal(document.getElementById('Modal-1'));
 const Modal2 = new bootstrap.Modal(document.getElementById('Modal-2'));
 const Modal3 = new bootstrap.Modal(document.getElementById('Modal-3'));
 const Modal4 = new bootstrap.Modal(document.getElementById('Modal-4'));
+const Modal5 = new bootstrap.Modal(document.getElementById('Modal-5'));
 
-const playModal1 = () => {
+const ModalClosebtn = document.querySelectorAll('.modal-closebtn');
+
+let ModalList = [Modal1 , Modal2, Modal3, Modal4, Modal5];
+
+ const closebtn = () => {
+    ModalList.forEach(modal => modal.hide());
+};
+
+ModalClosebtn.forEach(buttons =>{
+    buttons.addEventListener("click",closebtn)
+})
+
+const openModal1 = () => {
     Modal1.show();
 }
 
-const playModal2 = () => {
+const openModal2 = () => {
     Modal2.show();
 }
 
-const playModal3 = () => {
+const openModal3 = () => {
     Modal3.show();
 }
 
-const playModal4 = () => {
+const openModal4 = () => {
     Modal4.show();
+}
+
+const openModal5 = () => {
+    Modal5.show();
 }
